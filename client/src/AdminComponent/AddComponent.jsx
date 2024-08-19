651b0310b2dd9b215854cf916bf4d79c213f1b17
Ascenxio Blog <hassanyusuuf122@gmail.com>
2024-08-19 20:36:04 +0100

    just in

--- client/src/AdminComponent/AddComponent.jsx
+++ client/src/AdminComponent/AddComponent.jsx
@@ -13,12 +13,6 @@ function AddComponent() {
     }
 
   return (
-<<<<<<< HEAD
-    <div className=' bg-[#E7F0DC] w-full min-h-[100vh] flex justify-center items-center mt-[30px]'>
-
-        <div className=" min-h-[90vh] w-full lg:w-[80%] bg-[] pl-[20px] md-pl-[40px] lg:pl-[100px]">
-            <form action="localhost:5000/add" method='post'>
-=======
     <div className=' bg-blue-700 w-full min-h-[100vh] flex justify-center items-center'>
         <div className=" w-[max-content] bg-[#f3f9d2] p-3 absolute rounded-lg left-[50%] right-[50%] transition-[1s]" style={box}>
             <h2>Added Successfully</h2>
@@ -26,7 +20,6 @@ function AddComponent() {
 
         <div className=" min-h-[90vh] w-full lg:w-[80%] bg-fuchsia-500 pl-[20px] md-pl-[40px] lg:pl-[100px]">
             <form action="http://localhost:5000/add" method='post' onSubmit={move} encType="multipart/form-data">
->>>>>>> 34215d88caa6c4191a772f25cee62d81f2e917b2
                 
             <div className="">
                 <label htmlFor="">Restaurant Name</label> <br />
@@ -63,7 +56,7 @@ function AddComponent() {
                         <input type="checkbox" name="food_types" value="pastries"/>
                         <label htmlFor="">Pastries</label>
                     </span>
-
+                    
                     <span>
                         <input type="checkbox" name="food_types" value="local-food"/>
                         <label htmlFor="">Local Food</label>
@@ -113,13 +106,7 @@ function AddComponent() {
                 <input type="number" name='rating' min="1" max="5" required className=' h-[40px] w-[40%] rounded-[20px] bg-[#80808060] pl-2' />
             </div><br />
 
-<<<<<<< HEAD
-            <div className="">
-                <button type="submit" className=' h-[40px] w-[90%] bg-orange-500 rounded-[20px] font-bold'>Add Restaurant</button>
-            </div>
-=======
                 <button type="submit" className=' h-[40px] w-[90%] bg-orange-500 rounded-[20px]' >Add Restaurant</button>
->>>>>>> 34215d88caa6c4191a772f25cee62d81f2e917b2
             </form>
         </div>
     </div>
--- client/src/Restaurant1/Restaurant1Component.jsx
+++ client/src/Restaurant1/Restaurant1Component.jsx
@@ -1,93 +1,115 @@
-import React, { useEffect ,useState } from 'react'
-
+import React, { useEffect, useState } from "react";
+const imageBaseUrl = "http://localhost:5000"; // Base URL of your server
 
 function Restaurant1Component() {
-  let [holddata,setHolddata] = useState('')
+  let [holddata, setHolddata] = useState("");
 
-  useEffect(()=>{
-    fetch("http://localhost:5000/api/restaurants")//fetched from my server
-        .then((response)=>response.json()) //convert the response to json. (the response can be anything)
-        .then(json=>setHolddata(json)) //(then pass the converted data into a useState)
-        .catch(err=>console.log(err)) // if there is any error
-
-  },[])//Array of dependecies because we need the data to  be fetched only once.
+  useEffect(() => {
+    fetch("http://localhost:5000/api/restaurants") //fetched from my server
+      .then((response) => response.json()) //convert the response to json. (the response can be anything)
+      .then((json) => setHolddata(json)) //(then pass the converted data into a useState)
+      .catch((err) => console.log(err)); // if there is any error
+  }, []); //Array of dependecies because we need the data to  be fetched only once.
 
   return (
-    <div className=' min-h-[800px] bg-slate-600 grid grid-cols-3 gap-2'>
-      {
-        holddata? 
-        holddata.map((item)=>(
-          <>
-          <div className=' bg-gray-500 '>
-            <h2 >{item.name}</h2>
-            <img src={item.image} width={"300px"} className=' rounded-xl'/>
-            <p>{item.address}</p>
-          </div>
-          
-          </>
-          
-        )) : <p>Poor network...loading</p>
-      }
-      
-    </div>
-    // <div className=' bg-teal-300 h-full w-full'>
-    //   <div className=" mt-[120px] min-h-[500px] w-[95%] bg-[] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-    //   <div className=" h-[500px]  bg-white flex flex-col items-center ">
-    //             <div className=" border-[1px] border-[gray] rounded-[25px] overflow-hidden">
-    //             <div className=" h-[350px] w-[350px] md:w-[320px] lg:w-[410px] bg-red-800 ">
-    //               <img src="" alt="" />
-    //             </div>
-    //             <div className=" h-[90px]  bg-[] grid grid-cols-[70%,30%]">
-    //               <div className=" bg-[] flex flex-col justify-center pl-2">
-    //                 <h1 className=' font-bold text-[17px] mb-1'>Jollof & Co</h1>
-    //                 <p>Everyday Naija favourites</p>
-    //               </div>
-    //               <div className=" bg-[] flex justify-center items-center text-[18px]">
-    //                 <span className=' text-[#FF5A3C]'><u><a href="">View More</a></u></span>
-    //               </div>
-    //             </div>
-    //             </div>
+    // <div className=' min-h-[800px] bg-slate-600 grid grid-cols-3 gap-2 grid-rows-2'>
+    //       {holddata ? 
+    //       holddata.map((item) => (
+    //         <> 
+    //         <div>
+    //         <img src={`${imageBaseUrl}${item.image}`} alt={item.name} />
+    //           <h1>{item.address}</h1>
+    //           <p>{item.name}</p>
     //         </div>
+    //         </> 
+            
+    //       )):null }
 
 
-    //         <div className=" h-[500px]  bg-white flex flex-col items-center ">
-    //             <div className=" border-[1px] border-[gray] rounded-[25px] overflow-hidden">
-    //             <div className=" h-[350px] w-[350px] md:w-[320px] lg:w-[410px] bg-red-800 ">
-    //               <img src="" alt="" />
-    //             </div>
-    //             <div className=" h-[90px]  bg-[] grid grid-cols-[70%,30%]">
-    //               <div className=" bg-[] flex flex-col justify-center pl-2">
-    //                 <h1 className=' font-bold text-[17px] mb-1'>Jollof & Co</h1>
-    //                 <p>Everyday Naija favourites</p>
-    //               </div>
-    //               <div className=" bg-[] flex justify-center items-center text-[18px]">
-    //                 <span className=' text-[#FF5A3C]'><u><a href="">View More</a></u></span>
-    //               </div>
-    //             </div>
-    //             </div>
-    //         </div>
-
-
-    //         <div className=" h-[500px]  bg-white flex flex-col items-center ">
-    //             <div className=" border-[1px] border-[gray] rounded-[25px] overflow-hidden">
-    //             <div className=" h-[350px] w-[350px] md:w-[320px] lg:w-[410px] bg-red-800 ">
-    //               <img src="" alt="" />
-    //             </div>
-    //             <div className=" h-[90px]  bg-[] grid grid-cols-[70%,30%]">
-    //               <div className=" bg-[] flex flex-col justify-center pl-2">
-    //                 <h1 className=' font-bold text-[17px] mb-1'>Jollof & Co</h1>
-    //                 <p>Everyday Naija favourites</p>
-    //               </div>
-    //               <div className=" bg-[] flex justify-center items-center text-[18px]">
-    //                 <span className=' text-[#FF5A3C]'><u><a href="">View More</a></u></span>
-    //               </div>
-    //             </div>
-    //             </div>
-    //         </div>
-    //   </div>
-      
     // </div>
-  )
+    <div className=" bg-teal-300 h-full w-full">
+      <div className=" mt-[120px] min-h-[500px] w-[95%] bg-[] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+       
+    
+    {holddata ? 
+      holddata.map((item) => (
+        <> 
+
+
+<div className=" h-[500px]  bg-white flex flex-col items-center " >
+                 <div className=" border-[1px] border-[gray] rounded-[25px] overflow-hidden">
+                   <div className=" h-[350px] w-[350px] md:w-[320px] lg:w-[380px] bg-red-800 ">
+                    <img src={`${imageBaseUrl}${item.image}`} alt={item.name} />
+                     {console.log(item.image)}
+                   </div>
+                   <div className=" h-[90px]  bg-[] grid grid-cols-[70%,30%]">
+                     <div className=" bg-[] flex flex-col justify-center pl-2">
+                       <h1 className=" font-bold text-[17px] mb-1">
+                         {item.name}
+                       </h1>
+                       <p>{item.address}</p>
+                     </div>
+                     <div className=" bg-[] flex justify-center items-center text-[18px]">
+                       <span className=" text-[#FF5A3C]">
+                         <a href="" className=" underline">
+                           View More
+                         </a>
+                       </span>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+        {/* <div>
+        <img src={`${imageBaseUrl}${item.image}`} alt={item.name} />
+          <h1>{item.address}</h1>
+          <p>{item.name}</p>
+        </div> */}
+        </> 
+        
+      )):null }
+    
+    
+    
+    
+    
+    
+{/*     `
+    
+    {holddata ? (
+           holddata.map((item) => (
+             <>
+
+               <div className=" h-[500px]  bg-white flex flex-col items-center " >
+                 <div className=" border-[1px] border-[gray] rounded-[25px] overflow-hidden">
+                   <div className=" h-[350px] w-[350px] md:w-[320px] lg:w-[380px] bg-red-800 ">
+                     <img src={item.image} />
+                     {console.log(item.image)}
+                   </div>
+                   <div className=" h-[90px]  bg-[] grid grid-cols-[70%,30%]">
+                     <div className=" bg-[] flex flex-col justify-center pl-2">
+                       <h1 className=" font-bold text-[17px] mb-1">
+                         {item.name}
+                       </h1>
+                       <p>{item.address}</p>
+                     </div>
+                     <div className=" bg-[] flex justify-center items-center text-[18px]">
+                       <span className=" text-[#FF5A3C]">
+                         <a href="" className=" underline">
+                           View More
+                         </a>
+                       </span>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             </>
+           ))
+         ) : (
+           <p>Poor network...loading</p>
+         )}` */}
+      </div>
+    </div>
+  );
 }
 
-export default Restaurant1Component
+export default Restaurant1Component;
